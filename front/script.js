22e402a73fb5fd1da3902ea38efbdad9f3fd6f83
Emily2393 <emilie.rich4rd@gmail.com>
2023-09-29 10:39:09 +0200

    Script finalisation-style improvment

--- front/script.js
+++ front/script.js
@@ -50,22 +50,25 @@ function slider(category, number){
     category.addEventListener("scroll", infiniteScroll)
 }
 
-function Modal(pageOne, pageTwo){
-
-    
+function modal(page, page2){
     selected = document.querySelectorAll("img")
     for (let i = 1; i < selected.length; i++) {
         const openModal = () => {
-            for (let e=0; e < pageOne.length; e++){
-                if (selected[i].id == pageOne[e].id){
-                    createModal(pageOne[e])
+            for (let e=0; e < page.length; e++){
+                if (selected[i].id == page[e].id){
+                    console.log(page[e])
+                    createModal(page[e])
                 }
             }
-            
+            for (let e=0; e < page2.length; e++){
+                if (selected[i].id == page2[e].id){
+                    console.log(page2[e])
+                    createModal(page2[e])
+                }
+            }
         }
         selected[i].addEventListener("click", openModal)
     }
-
 }
 
 function createModal(movie){
@@ -79,6 +82,9 @@ function createModal(movie){
     let button = document.createElement("button")
     button.className = "Modal__flex__button"
     button.textContent = "X"
+    button.addEventListener('click', function(){
+        section.style.display = "none"
+    })
     child(divFlex, button)
     let inner = document.createElement("div")
     child(divFlex, inner)
@@ -97,9 +103,8 @@ function createModal(movie){
 async function checkImg(url) {
     try {
         response = await fetch(url)
-        console.log(response)
     } catch(erreur) {
-        console.log(erreur)
+        console.log("Non trouvé")
         return false
     }
     return response
@@ -107,6 +112,7 @@ async function checkImg(url) {
 
 async function publishCarrousel(pictures, nextPictures, number) {
     newPage = 0
+    const page = document.querySelector(".Page")
     const container = document.querySelector(`.Category${number}__carousel`)
     for (let i = 0; i < 7; i++) {
         let li = document.createElement("li")
@@ -130,25 +136,40 @@ async function publishCarrousel(pictures, nextPictures, number) {
             newPage += 1
         }
         else {
-            img.src = pictures[i].image_url
+            let url = pictures[i].image_url
+            img.src = url
             img.id = pictures[i].id
+            test = await checkImg(url)
+            if (test.status === 404 || !test){
+                url = pictures[i + 1].image_url
+                img.src = url
+                img.id = pictures[i + 1].id
+                i += 1
+            }
         }
     }
+    page.style.display = "flex"
     slider(container, number)
 }
 
 
 async function launch() {
     const bestMovie = await getData("http://127.0.0.1:8000/api/v1/titles/?sort_by=-imdb_score")
-    const cat1Pictures = await getData("http://127.0.0.1:8000/api/v1/titles/?genre_contains=Fantasy&sort_by=-imdb_score")
-    const cat2Pictures = await getData("http://127.0.0.1:8000/api/v1/titles/?genre_contains=thriller&sort_by=-imdb_score")
-    const cat1NextPage = await getData("http://127.0.0.1:8000/api/v1/titles/?genre_contains=Fantasy&page=2&sort_by=-imdb_score")
-    const cat2NextPage = await getData("http://127.0.0.1:8000/api/v1/titles/?genre_contains=thriller&page=2&sort_by=-imdb_score")
+    const cat3Pictures = await getData("http://127.0.0.1:8000/api/v1/titles/?genre_contains=Fantasy&sort_by=-imdb_score")
+    const cat2Pictures = await getData("http://127.0.0.1:8000/api/v1/titles/?genre_contains=adventure&sort_by=-imdb_score")
+    const cat3NextPage = await getData("http://127.0.0.1:8000/api/v1/titles/?genre_contains=Fantasy&page=2&sort_by=-imdb_score")
+    const cat2NextPage = await getData("http://127.0.0.1:8000/api/v1/titles/?genre_contains=adventure&page=2&sort_by=-imdb_score")
+    const cat1Pictures = await getData("http://127.0.0.1:8000/api/v1/titles/?genre_contains=crime&sort_by=-imdb_score")
+    const cat1NextPage = await getData("http://127.0.0.1:8000/api/v1/titles/?genre_contains=crime&page=2&sort_by=-imdb_score")
     publishBestMovies(bestMovie.results[0])
     await publishCarrousel(cat1Pictures.results, cat1NextPage.results, 1)
     await publishCarrousel(cat2Pictures.results, cat2NextPage.results, 2)
-    Modal(cat1Pictures.results, cat1NextPage.results)
+    await publishCarrousel(cat3Pictures.results, cat3NextPage.results, 3)
+    modal(cat1Pictures.results, cat1NextPage.results)
+    modal(cat2Pictures.results, cat2NextPage.results)
+    modal(cat3Pictures.results, cat3NextPage.results)
 }
 
 launch()
+
 
